bcaa7c8830a03d20edcbfc7154fc06c88aca40df
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2022-02-16 16:56:24 +0000

    Project startup files

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const http = require('http');
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+
+// Routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname + 'public\index.html'));
+})
+// Server setup
+const server = http.createServer(app);
+server.listen(4000, () => {
+    console.log('listening on port 4000');
+});
