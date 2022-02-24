53241f20bfdc039de94c67ff1fcc195b5df1558f
Telle Abbott <telleabbott@gmail.com>
2022-02-24 13:38:17 +0000

    Sorting js files to organise code better

---
+++ public/js/dataRead.js
@@ -0,0 +1,55 @@
+async function getData() {
+    const response = await fetch('data/UK_data.csv');
+    const data = await response.text();
+    // make an array for all filds
+    const date = [],
+        total_cases = [], 
+        new_cases = [], 
+        new_cases_smoothed = [],
+        total_deaths = [],
+        new_deaths = [],
+        new_deaths_smoothed = [],
+        total_cases_per_million = [],
+        new_cases_per_million = [],
+        new_cases_smoothed_per_million = [],
+        total_deaths_per_million = [],
+        new_deaths_per_million = [],
+        new_deaths_smoothed_per_million = [],
+        reproduction_rate = [];
+    // placing the data within the correct array
+    const rows = data.split('\n').slice(1);
+    rows.forEach(row => {
+        const d = row.split(',');
+        date.push(d[0]);
+        total_cases.push(d[1]);
+        new_cases.push(d[2]);
+        new_cases_smoothed.push(d[3]);
+        total_deaths.push(d[4]);
+        new_deaths.push(d[5]);
+        new_deaths_smoothed.push(d[6]);
+        total_cases_per_million.push(d[7]);
+        new_cases_per_million.push(d[8]);
+        new_cases_smoothed_per_million.push(d[9]);
+        total_deaths_per_million.push(d[10]);
+        new_deaths_per_million.push(d[11]);
+        new_deaths_smoothed_per_million.push(d[12]);
+        reproduction_rate.push(d[13]);
+    });
+    // returing these arrays
+    return { 
+        date,
+        total_cases, 
+        new_cases, 
+        new_cases_smoothed,
+        total_deaths,
+        new_deaths,
+        new_deaths_smoothed,
+        total_cases_per_million,
+        new_cases_per_million,
+        new_cases_smoothed_per_million,
+        total_deaths_per_million,
+        new_deaths_per_million,
+        new_deaths_smoothed_per_million,
+        reproduction_rate
+    };
+}
--- public/js/index.js
+++ public/js/index.js
@@ -6,12 +6,14 @@
     Move chart config into its own file
 */
 
+
+
 window.addEventListener('load', setup);
 async function setup() {
     const ctx = document.getElementById('myChart').getContext('2d');
     const covidData = await getData();
     const myChart = new Chart(ctx, {
-        type: 'bar',
+        type: 'line',
         data: {
             labels: covidData.date,
             datasets: [
@@ -51,60 +53,4 @@ async function setup() {
         },
         options: { responsive: false }
     });
-}
-
-async function getData() {
-    const response = await fetch('data/UK_data.csv');
-    const data = await response.text();
-    // make an array for all filds
-    const date = [],
-        total_cases = [], 
-        new_cases = [], 
-        new_cases_smoothed = [],
-        total_deaths = [],
-        new_deaths = [],
-        new_deaths_smoothed = [],
-        total_cases_per_million = [],
-        new_cases_per_million = [],
-        new_cases_smoothed_per_million = [],
-        total_deaths_per_million = [],
-        new_deaths_per_million = [],
-        new_deaths_smoothed_per_million = [],
-        reproduction_rate = [];
-    // placing the data within the correct array
-    const rows = data.split('\n').slice(1);
-    rows.forEach(row => {
-        const d = row.split(',');
-        date.push(d[0]);
-        total_cases.push(d[1]);
-        new_cases.push(d[2]);
-        new_cases_smoothed.push(d[3]);
-        total_deaths.push(d[4]);
-        new_deaths.push(d[5]);
-        new_deaths_smoothed.push(d[6]);
-        total_cases_per_million.push(d[7]);
-        new_cases_per_million.push(d[8]);
-        new_cases_smoothed_per_million.push(d[9]);
-        total_deaths_per_million.push(d[10]);
-        new_deaths_per_million.push(d[11]);
-        new_deaths_smoothed_per_million.push(d[12]);
-        reproduction_rate.push(d[13]);
-    });
-    // returing these arrays
-    return { 
-        date,
-        total_cases, 
-        new_cases, 
-        new_cases_smoothed,
-        total_deaths,
-        new_deaths,
-        new_deaths_smoothed,
-        total_cases_per_million,
-        new_cases_per_million,
-        new_cases_smoothed_per_million,
-        total_deaths_per_million,
-        new_deaths_per_million,
-        new_deaths_smoothed_per_million,
-        reproduction_rate
-    };
 }
