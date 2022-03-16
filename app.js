5912c6f8a14939b340225af1a10c9f0949b3fe31
Telle Abbott <telleabbott@gmail.com>
2022-03-16 23:18:58 +0000

    link to readData.js in app

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const http = require('http');
 const app = express();
 
+const dataRead = require('./server/readData.js');
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
