8fc9bd2caa94293542f3376c7b6897cfc6a16002
Telle Abbott <telleabbott@gmail.com>
2022-03-17 05:08:02 +0000

    Removing unnecessary files and code

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const http = require('http');
 const app = express();
-const csv = require('csv-parser');
-const fs = require('fs');
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
@@ -10,22 +8,9 @@ app.use(express.static('public'));
 
 // Routes
 app.get('/', (req, res) => {
-    csvData()
     res.sendFile(path.join(__dirname + 'public\index.html'));
 })
-csvData();
-function csvData() {
-    const data = [];
-    fs.createReadStream('UK_data.csv')
-        .pipe(csv())
-        .on('data', (row) => {
-            data[0] = row;
-            console.log(data)
-        })
-        .on('end', () => {
-            console.log();
-        });
-}
+
 // Server setup
 const server = http.createServer(app);
 server.listen(4000, () => {
