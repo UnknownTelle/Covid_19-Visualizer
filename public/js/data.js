e79aab4d92b9bdc2c0565b8246b971ea08a5029b
Telle Abbott <telleabbott@gmail.com>
2022-03-23 15:07:14 +0000

    removed csv picker

--- public/js/data.js
+++ public/js/data.js
@@ -4,38 +4,16 @@
 | Date:   17 March 2022
 | Description: Read and sort throgh csv data
 ========================================== */
-
-// user can pick what covid data they wish to see based on year
-const getFile = async (year) => {
-    let response;
-    let data;
-    switch (year) {
-        case 2020:
-            response = await fetch('data/UK_data.csv');
-            data = await response.text(); 
-            break;
-        case 2021:
-            response = await fetch('data/UK_data.csv');
-            data = await response.text();
-            break;
-        case 2022:
-            response = await fetch('data/UK_data.csv');
-            data = await response.text();  
-            break;
-        default: data = null; break;
-    }
-    return data
-}
-
-const sortFile = async (year) => {
+const sortFile = async () => {
     // make the arrays
     const date = [], total_cases = [], new_cases = [], new_cases_smoothed = [],
-    total_deaths = [], new_deaths = [], new_deaths_smoothed = [], 
-    total_cases_per_million = [], new_cases_per_million = [], 
-    new_cases_smoothed_per_million = [], total_deaths_per_million = [],
-    new_deaths_per_million = [], new_deaths_smoothed_per_million = [], reproduction_rate = [];
+        total_deaths = [], new_deaths = [], new_deaths_smoothed = [],
+        total_cases_per_million = [], new_cases_per_million = [],
+        new_cases_smoothed_per_million = [], total_deaths_per_million = [],
+        new_deaths_per_million = [], new_deaths_smoothed_per_million = [], reproduction_rate = [];
     // read in data
-    const data = await getFile(year);
+    const response = await fetch('data/UK_data.csv');
+    const data = await response.text();
     // sort though data
     const rows = data.split('\n').slice(1);
     // place data within correct array
