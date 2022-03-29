c516352678e5f1ec500215e301bd710e4e6cf4ca
Telle Abbott <telleabbott@gmail.com>
2022-03-29 09:45:30 +0100

    minor changes

--- public/js/chart.js
+++ public/js/chart.js
@@ -9,90 +9,91 @@ function getRandomColor() {
     return color;
 }
 let chart ;
+let chartData = [];
 async function drawChart(chartChoice) {
     // Setup Block
     const covidData = await sortFile();
     const data = {
         labels: covidData.date,
         datasets: [
-            {
-                label: 'Total Cases',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.total_cases,
-            },
-            {
-                label: 'New Cases',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.new_cases,
-            },
-            {
-                label: 'New Cases Smoothed',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.new_cases_smoothed,
-            },
-            {
-                label: 'Total Deaths',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.total_deaths,
-            },
-            {
-                label: 'New Deaths',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.new_deaths,
-            },
-            {
-                label: 'New Deaths Smoothed',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.new_deaths_smoothed,
-            },
-            {
-                label: 'Total Cases Per Million',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.total_cases_per_million,
-            },
-            {
-                label: 'New Cases Per Million',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.new_cases_per_million,
-            },
-            {
-                label: 'New Cases Smoothed Per Million',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.new_cases_smoothed_per_million,
-            },
-            {
-                label: 'Total Deaths Per Milllion',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.total_deaths_per_million,
-            },
-            {
-                label: 'New Deaths Per Million',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.new_deaths_per_million,
-            },
-            {
-                label: 'New Deaths Smoothed Per Million',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.new_deaths_smoothed_per_million,
-            },
-            {
-                label: 'Reproduction Rate',
-                backgroundColor: getRandomColor(),
-                fill: false,
-                data: covidData.reproduction_rate,
-            },
+            // {
+            //     label: 'Total Cases',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.total_cases,
+            // },
+            // {
+            //     label: 'New Cases',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.new_cases,
+            // },
+            // {
+            //     label: 'New Cases Smoothed',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.new_cases_smoothed,
+            // },
+            // {
+            //     label: 'Total Deaths',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.total_deaths,
+            // },
+            // {
+            //     label: 'New Deaths',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.new_deaths,
+            // },
+            // {
+            //     label: 'New Deaths Smoothed',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.new_deaths_smoothed,
+            // },
+            // {
+            //     label: 'Total Cases Per Million',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.total_cases_per_million,
+            // },
+            // {
+            //     label: 'New Cases Per Million',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.new_cases_per_million,
+            // },
+            // {
+            //     label: 'New Cases Smoothed Per Million',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.new_cases_smoothed_per_million,
+            // },
+            // {
+            //     label: 'Total Deaths Per Milllion',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.total_deaths_per_million,
+            // },
+            // {
+            //     label: 'New Deaths Per Million',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.new_deaths_per_million,
+            // },
+            // {
+            //     label: 'New Deaths Smoothed Per Million',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.new_deaths_smoothed_per_million,
+            // },
+            // {
+            //     label: 'Reproduction Rate',
+            //     backgroundColor: getRandomColor(),
+            //     fill: false,
+            //     data: covidData.reproduction_rate,
+            // },
         ]
     }
     // Config Block
@@ -135,3 +136,22 @@ const chartType = (type) => {
           break;
       }
 }
+
+async function addData(dataName) {
+    const gettingData = await sortFile();
+    console.log('this is dateName val ' + dataName)
+    switch(dataName) {
+        case 'total_cases':
+            chart.data.datasets.data.push(gettingData.total_cases)
+    }
+    //chart.data.labels.push(gettingData.date); // This push label
+    chart.data.datasets.forEach((dataset) => {
+        dataset.data.push(gettingData.reproduction_rate) // This is not pushing anything
+    });
+    chart.update();
+}
+
+document.getElementById('data-select').addEventListener('change', function () {
+    addData(this.value);
+    console.log('You selected: ', this.value);
+});
