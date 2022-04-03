8264553ac19eb03ae315c2f7ab2ee0f34f83891b
Telle Abbott <telleabbott@gmail.com>
2022-04-03 15:22:09 +0100

    set up a connection to the server side

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,15 @@
-window.addEventListener('load', e => {
-    drawChart('line');
-})
+document.getElementById('data-select').addEventListener('change', function () {
+    dataPost(this.value);
+});
+const dataPost = async (value) => {
+    const data = { value }
+const options = {
+    method: 'POST',
+    mode: 'cors',
+    headers: {'Content-Type': 'application/json'},
+    body: JSON.stringify(data)
+};
+const res = await fetch('/', options)
+const response = await res.json();
+console.log(response);
+}
