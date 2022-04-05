f701ba3d86e93132ac1bab687ae582eb331b2a3e
Telle Abbott <telleabbott@gmail.com>
2022-04-05 18:39:22 +0100

    removed getData and added link to data.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const fs = require('fs');
 const path = require('path');
 const app = express();
 
+const getData = require('./server/data')
+
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
@@ -26,61 +28,6 @@ app.post('/', (req, res) => {
         })
     });
 })
-// attempt to move to its own file
-const getData = (val) => {
-    return new Promise((res, rej) => {
-        let results = [];
-        fs.createReadStream(path.join(__dirname, '/server/data/UK_data.csv'))
-            .pipe(csv())
-            .on('data', (row) => {
-                switch (val) {
-                    case 'date':
-                        results.push(row.date);
-                        break;
-                    case 'total_cases':
-                        results.push(row.total_cases);
-                        break;
-                    case 'new_cases':
-                        results.push(row.new_cases);
-                        break;
-                    case 'new_cases_smoothed':
-                        results.push(row.new_cases_smoothed);
-                        break;
-                    case 'total_deaths':
-                        results.push(row.total_deaths);
-                        break;
-                    case 'new_deaths':
-                        results.push(row.new_deaths);
-                        break;
-                    case 'new_deaths_smoothed':
-                        results.push(row.new_deaths_smoothed);
-                        break;
-                    case 'total_cases_per_million':
-                        results.push(row.total_cases_per_million);
-                        break;
-                    case 'new_cases_per_million':
-                        results.push(row.new_cases_per_million);
-                        break;
-                    case 'new_cases_smoothed_per_million':
-                        results.push(row.new_cases_smoothed_per_million);
-                        break;
-                    case 'total_deaths_per_million':
-                        results.push(row.total_deaths_per_million);
-                        break;
-                    case 'new_deaths_per_million':
-                        results.push(row.new_deaths_per_million);
-                        break;
-                    case 'new_deaths_smoothed_per_million':
-                        results.push(row.new_deaths_smoothed_per_million);
-                        break;
-                    case 'reproduction_rate':
-                        results.push(row.reproduction_rate);
-                        break;
-                }
-            })
-            .on('end', () => {res(results)})
-    })
-}
 
 // Server setup
 const server = http.createServer(app);
