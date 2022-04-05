d2f63ce7b6e2c90ffb2a412ae059e831d7979db9
Telle Abbott <telleabbott@gmail.com>
2022-04-05 18:07:57 +0100

    filtred data gets sent to client via getData()

--- app.js
+++ app.js
@@ -19,21 +19,66 @@ app.get('/', (req, res) => {
 
 app.post('/', (req, res) => {
     const val = req.body.value
-    dataPromise().then((result) => {
+    getData(val).then((result) => {
         res.json({
-        sataus: 'success',
-        value: result
+            sataus: 'success',
+            value: result
         })
     });
 })
 // attempt to move to its own file
-async function dataPromise() {
+const getData = (val) => {
     return new Promise((res, rej) => {
         let results = [];
         fs.createReadStream(path.join(__dirname, '/server/data/UK_data.csv'))
             .pipe(csv())
-            .on('data', (row) => { results.push(row) })
-            .on('end', () => { res(results) })
+            .on('data', (row) => {
+                switch (val) {
+                    case 'date':
+                        results.push(row.date);
+                        break;
+                    case 'total_cases':
+                        results.push(row.total_cases);
+                        break;
+                    case 'new_cases':
+                        results.push(row.new_cases);
+                        break;
+                    case 'new_cases_smoothed':
+                        results.push(row.new_cases_smoothed);
+                        break;
+                    case 'total_deaths':
+                        results.push(row.total_deaths);
+                        break;
+                    case 'new_deaths':
+                        results.push(row.new_deaths);
+                        break;
+                    case 'new_deaths_smoothed':
+                        results.push(row.new_deaths_smoothed);
+                        break;
+                    case 'total_cases_per_million':
+                        results.push(row.total_cases_per_million);
+                        break;
+                    case 'new_cases_per_million':
+                        results.push(row.new_cases_per_million);
+                        break;
+                    case 'new_cases_smoothed_per_million':
+                        results.push(row.new_cases_smoothed_per_million);
+                        break;
+                    case 'total_deaths_per_million':
+                        results.push(row.total_deaths_per_million);
+                        break;
+                    case 'new_deaths_per_million':
+                        results.push(row.new_deaths_per_million);
+                        break;
+                    case 'new_deaths_smoothed_per_million':
+                        results.push(row.new_deaths_smoothed_per_million);
+                        break;
+                    case 'reproduction_rate':
+                        results.push(row.reproduction_rate);
+                        break;
+                }
+            })
+            .on('end', () => {res(results)})
     })
 }
 
