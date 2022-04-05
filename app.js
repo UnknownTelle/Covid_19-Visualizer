9647e4589e2930cf3b2d8d46ef34b5b1bcbb6b82
Telle Abbott <telleabbott@gmail.com>
2022-04-05 18:40:31 +0100

    removed code thats not needed

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
-const csv = require('csv-parser');
-const fs = require('fs');
 const path = require('path');
 const app = express();
 
