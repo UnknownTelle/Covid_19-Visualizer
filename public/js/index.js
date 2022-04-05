2f5595dc3b306c9e3dd6a96579d6df0cf9fcb5ca
Telle Abbott <telleabbott@gmail.com>
2022-04-05 18:07:38 +0100

    filtred data gets sent to client via getData()

--- public/js/index.js
+++ public/js/index.js
@@ -3,13 +3,13 @@ document.getElementById('data-select').addEventListener('change', function () {
 });
 const dataPost = async (value) => {
     const data = { value }
-const options = {
-    method: 'POST',
-    mode: 'cors',
-    headers: {'Content-Type': 'application/json'},
-    body: JSON.stringify(data)
-};
-const res = await fetch('/', options)
-const response = await res.json();
-console.log(response);
+    const options = {
+        method: 'POST',
+        mode: 'cors',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(data)
+    };
+    const res = await fetch('/', options)
+    const response = await res.json();
+    console.log(response);
 }
