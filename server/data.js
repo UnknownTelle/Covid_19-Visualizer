e11592c25a239772b30411f6c1c651437b5877e5
Telle Abbott <telleabbott@gmail.com>
2022-04-06 10:46:15 +0100

    sending the data with the data to client side

--- server/data.js
+++ server/data.js
@@ -7,50 +7,23 @@ module.exports = function getData(val) {
         fs.createReadStream(path.join(__dirname, '/data/UK_data.csv'))
             .pipe(csv())
             .on('data', (row) => {
+                let data;
                 switch (val) {
-                    case 'date':
-                        results.push(row.date);
-                        break;
-                    case 'total_cases':
-                        results.push(row.total_cases);
-                        break;
-                    case 'new_cases':
-                        results.push(row.new_cases);
-                        break;
-                    case 'new_cases_smoothed':
-                        results.push(row.new_cases_smoothed);
-                        break;
-                    case 'total_deaths':
-                        results.push(row.total_deaths);
-                        break;
-                    case 'new_deaths':
-                        results.push(row.new_deaths);
-                        break;
-                    case 'new_deaths_smoothed':
-                        results.push(row.new_deaths_smoothed);
-                        break;
-                    case 'total_cases_per_million':
-                        results.push(row.total_cases_per_million);
-                        break;
-                    case 'new_cases_per_million':
-                        results.push(row.new_cases_per_million);
-                        break;
-                    case 'new_cases_smoothed_per_million':
-                        results.push(row.new_cases_smoothed_per_million);
-                        break;
-                    case 'total_deaths_per_million':
-                        results.push(row.total_deaths_per_million);
-                        break;
-                    case 'new_deaths_per_million':
-                        results.push(row.new_deaths_per_million);
-                        break;
-                    case 'new_deaths_smoothed_per_million':
-                        results.push(row.new_deaths_smoothed_per_million);
-                        break;
-                    case 'reproduction_rate':
-                        results.push(row.reproduction_rate);
-                        break;
+                    case 'total_cases': data = {total_cases : row.total_cases}; break;
+                    case 'new_cases': data = {new_cases: row.new_cases}; break;
+                    case 'new_cases_smoothed': data = {new_cases_smoothed: row.new_cases_smoothed}; break;
+                    case 'total_deaths': data = {total_deaths: row.total_deaths}; break;
+                    case 'new_deaths': data = {new_deaths: row.new_deaths}; break;
+                    case 'new_deaths_smoothed': data = {new_deaths_smoothed: row.new_deaths_smoothed}; break;
+                    case 'total_cases_per_million': data = {total_cases_per_million: row.total_cases_per_million}; break;
+                    case 'new_cases_per_million': data = {new_cases_per_million: row.new_cases_per_million}; break;
+                    case 'new_cases_smoothed_per_million': data = {new_cases_smoothed_per_million: row.new_cases_smoothed_per_million}; break;
+                    case 'total_deaths_per_million': data = {total_deaths_per_million: row.total_deaths_per_million}; break;
+                    case 'new_deaths_per_million': data = {new_deaths_per_million: row.new_deaths_per_million}; break;
+                    case 'new_deaths_smoothed_per_million': data = {new_deaths_smoothed_per_million: row.new_deaths_smoothed_per_million}; break;
+                    case 'reproduction_rate': data = {reproduction_rate: row.reproduction_rate}; break;
                 }
+                results.push({date: row.date, value: data})
             })
             .on('end', () => {res(results)})
     })
