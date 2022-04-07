1b7a203a9dee0fff7cdc91647d1a7e978d5ee1fe
Telle Abbott <telleabbott@gmail.com>
2022-04-07 09:56:12 +0100

    changes to adding date to array

--- server/data.js
+++ server/data.js
@@ -11,47 +11,47 @@ module.exports = function getData(val) {
                 let value;
                 switch (val) {
                     case 'total_cases':
-                        value = { total_cases: row.total_cases };
+                        value = row.total_cases;
                         break;
                     case 'new_cases':
-                        value = { new_cases: row.new_cases };
+                        value = row.new_cases;
                         break;
                     case 'new_cases_smoothed':
-                        value = { new_cases_smoothed: row.new_cases_smoothed };
+                        value = row.new_cases_smoothed;
                         break;
                     case 'total_deaths':
-                        value = { total_deaths: row.total_deaths };
+                        value = row.total_deaths;
                         break;
                     case 'new_deaths':
-                        value = { new_deaths: row.new_deaths };
+                        value = row.new_deaths;
                         break;
                     case 'new_deaths_smoothed':
-                        value = { new_deaths_smoothed: row.new_deaths_smoothed };
+                        value = row.new_deaths_smoothed;
                         break;
                     case 'total_cases_per_million':
-                        value = { total_cases_per_million: row.total_cases_per_million };
+                        value = row.total_cases_per_million;
                         break;
                     case 'new_cases_per_million':
-                        value = { new_cases_per_million: row.new_cases_per_million };
+                        value = row.new_cases_per_million;
                         break;
                     case 'new_cases_smoothed_per_million':
-                        value = { new_cases_smoothed_per_million: row.new_cases_smoothed_per_million };
+                        value = row.new_cases_smoothed_per_million;
                         break;
                     case 'total_deaths_per_million':
-                        value = { total_deaths_per_million: row.total_deaths_per_million };
+                        value = row.total_deaths_per_million;
                         break;
                     case 'new_deaths_per_million':
-                        value = { new_deaths_per_million: row.new_deaths_per_million };
+                        value = row.new_deaths_per_million;
                         break;
                     case 'new_deaths_smoothed_per_million':
-                        value = { new_deaths_smoothed_per_million: row.new_deaths_smoothed_per_million };
+                        value = row.new_deaths_smoothed_per_million;
                         break;
                     case 'reproduction_rate':
-                        value = { reproduction_rate: row.reproduction_rate };
+                        value = row.reproduction_rate ;
                         break;
                 }
-                results.push({ date: row.date, value })
+                results.push({"Day":{"date": row.date, value}})
             })
-            .on('end', () => { console.log(results[1]); res(results) })
+            .on('end', () => { res(results) })
     })
 }
