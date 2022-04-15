01030667601bb70d2a86cfabc3c4d445345d92cb
Telle Abbott <telleabbott@gmail.com>
2022-04-15 19:28:45 +0100

    temp file

---
+++ public/js/treemap.js
@@ -0,0 +1,17 @@
+/* 
+If you have time to complet
+Can use a chart.js plugin to complet
+for the doc:
+https://chartjs-chart-treemap.pages.dev/usage.html#dividers
+
+To use just add this into the HTML under the chart.js CDN:
+<script src="https://cdn.jsdelivr.net/npm/chartjs-chart-treemap@2.0.2/dist/chartjs-chart-treemap.min.js"></script>
+
+Video to add treemap:
+https://www.youtube.com/watch?v=fN-M7viEIy8
+Video to help with colour:
+https://www.youtube.com/watch?v=j8yiJFbCPI0
+Video to help format labels:
+https://www.youtube.com/watch?v=GLnRak17GFM
+
+ */
