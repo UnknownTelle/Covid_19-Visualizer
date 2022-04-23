fa088126dd87e3462e151bdd2a2c6c68baf4bfe3
Telle Abbott <57302004+UnknownTelle@users.noreply.github.com>
2022-04-23 19:48:30 +0100

    file clean up

--- public/js/chart.js
+++ public/js/chart.js
@@ -1,7 +1,8 @@
 /*-----------------------------------------------------------------
 Description: This javaScript file takes care of the line and bar
-charts. Within this file you will also have the add data, 
-remove data and filter data methods.
+charts. It wil use Chart.js libray to create the chart. There is 
+a function that will allow for the user to add and remove data 
+from the line or bar chart.
 ------------------------------------------------------------------*/
 
 // Create chart
@@ -66,7 +67,6 @@ const addData = async (key) => {
         borderColor: color
     }
     chart.data.datasets.push(newDataset); // Push new data
-    addRadarData(key);
     filterByDate(); // Check to see if dates are correct
 }
 
@@ -74,31 +74,6 @@ const addData = async (key) => {
 const removeData = (value) => {
     const index = chart.data.datasets.findIndex(x => x.label === value); // Get index
     chart.data.datasets.splice(index, 1) // Remove by index
-    chart.update();
-    updateTable();
-}
-
-// Filters the data displayed by the inputted dates
-const filterByDate = async () => {
-    // Get current inputted date values
-    const startDate = document.getElementById('start_date').value;
-    const endDate = document.getElementById('end_date').value;
-
-    // Gets the dates and filter them depending on inputted data
-    const dates = await getData('date', 'return'); // Get dates
-    const indexOfStartDate = dates.findIndex(sd => sd === startDate); // Get indexs
-    const indexOfEndDate = dates.findIndex(ed => ed === endDate); // Get indexs
-    const filterDate = dates.slice(indexOfStartDate, indexOfEndDate + 1); // Filter
-    chart.config.data.labels = filterDate; // Replace data
-
-    // Gets chart data and filters it depending on inputted data
-    const datasets = [...chart.data.datasets] // Copy chart data
-    for (var i = 0; i < datasets.length; i++) {
-        const label = datasets[i].label // Get lable
-        const data = await getData(label, 'return'); // Gets data using lable
-        const filterDateValue = data.slice(indexOfStartDate, indexOfEndDate + 1); // Filter data
-        chart.config.data.datasets[i].data = filterDateValue; // Replace data
-    }
     chart.update();
     updateTable();
 }
